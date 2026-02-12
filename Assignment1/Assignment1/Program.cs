65ab545d0cb12e6b594bc7cd703038cc8164fe27
Jenny TRIEU <jtrieu7@nait.ca>
2026-02-11 21:24:30 -0700

    recreated in new program

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare Varibles 
+            double initial;
+            double ratePercent;
+            double monthlyRate;
+            double futureValue;
+            int months;
+            int years;
+
+            //Prompt user to put in the input
+            Console.WriteLine("---FUTURE VALUE CALCULATOR--");
+            Console.Write("Enter Initial winnings: ");
+            initial = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter Interest rate %: ");
+            ratePercent = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter how many years you want to invest: ");
+            years = int.Parse(Console.ReadLine());
+
+            //Calculations
+            monthlyRate = (ratePercent / 100) / 12;
+            months = years * 12;
+
+            futureValue = initial * Math.Pow(1 + monthlyRate, months);
+
+            //Display the results
+            Console.WriteLine("---Summary---");
+            Console.WriteLine($"Initial: {initial:C2}");
+            Console.WriteLine($"Rate: {ratePercent}%");
+            Console.WriteLine($"Years: {years}");
+            Console.WriteLine($"Future Value: {futureValue:C2}");
+            Console.WriteLine();
+            Console.WriteLine($"The future value amount of {initial:C2} in {years} is {futureValue:C2}. Thank you goodbye.");
+            Console.WriteLine();
+            Console.WriteLine("---NOTES---");
+            Console.WriteLine("-The interest rate was converted to monthly. ");
+            Console.WriteLine("-The years were converted to months. ");
+        }
+    }
+}
